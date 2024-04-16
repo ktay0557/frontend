dca7d4ac68ba4e164ee32deb01f81dcc38e7f009
Kayleigh Taylor <ktay0557@gmail.com>
2024-04-16 14:51:24 +0000

    Add isAdmin to NavBar for advert creation

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -12,6 +12,7 @@ import { axiosReq } from "../api/axiosDefaults";
 const NavBar = () => {
     const currentUser = useCurrentUser();
     const setCurrentUser = useSetCurrentUser();
+    const isAdmin = currentUser && currentUser.is_staff;
 
     const handleSignOut = async () => {
         try {
@@ -59,7 +60,7 @@ const NavBar = () => {
             to="/"
             onClick={handleSignOut}
         >
-            <i class="fa-solid fa-right-from-bracket"></i>Sign Out
+            <i className="fa-solid fa-right-from-bracket"></i>Sign Out
         </NavLink>
         <NavLink
             className={styles.NavLink}
@@ -101,7 +102,7 @@ const NavBar = () => {
                         <img src={logo} alt="logo" height="45" />
                     </Navbar.Brand>
                 </NavLink>
-                {currentUser && addAdvertIcon}
+                {isAdmin ? addAdvertIcon : null}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
