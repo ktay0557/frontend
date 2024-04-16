84029c9dd0abb5163673c24bdadd59803c99769a
Kayleigh Taylor <ktay0557@gmail.com>
2024-04-16 13:22:25 +0000

    Add NavBar icons and functionality to logout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,14 +5,78 @@ import Nav from "react-bootstrap/Nav";
 import logo from "../assets/logo.png";
 import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import { useCurrentUser, useSetCurrentUser } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
+import { axiosReq } from "../api/axiosDefaults";
 
 const NavBar = () => {
     const currentUser = useCurrentUser();
+    const setCurrentUser = useSetCurrentUser();
 
-    const loggedInIcons = <>{currentUser?.username}</>
+    const handleSignOut = async () => {
+        try {
+            await axiosReq.post("dj-rest-auth/logout/");
+            setCurrentUser(null);
+        } catch (err) {
+            console.log(err);
+        }
+    };
+
+    const addAdvertIcon = (
+        <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/adverts/create"
+        >
+            <i class="fa-regular fa-square-plus"></i>Create Advert
+        </NavLink>
+    )
+
+    const loggedInIcons = <>
+        <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/cats"
+        >
+            <i class="fa-solid fa-cat"></i>The Cats
+        </NavLink>
+        <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/liked"
+        >
+            <i class="fa-solid fa-thumbs-up"></i>Liked
+        </NavLink>
+        <NavLink
+            className={styles.NavLink}
+            activeClassName={styles.Active}
+            to="/about"
+        >
+            <i class="fa-solid fa-address-card"></i>About
+        </NavLink>
+        <NavLink
+            className={styles.NavLink}
+            to="/"
+            onClick={handleSignOut}
+        >
+            <i class="fa-solid fa-right-from-bracket"></i>Sign Out
+        </NavLink>
+        <NavLink
+            className={styles.NavLink}
+            to={`/profiles/${currentUser?.profile_id}`}
+        >
+            <Avatar src={currentUser?.profile_image} text={currentUser?.username} height={40} />
+        </NavLink>
+    </>
     const loggedOutIcons = (
         <>
+            <NavLink
+                className={styles.NavLink}
+                activeClassName={styles.Active}
+                to="/about"
+            >
+                <i class="fa-solid fa-address-card"></i>About
+            </NavLink>
             <NavLink
                 className={styles.NavLink}
                 activeClassName={styles.Active}
@@ -30,13 +94,14 @@ const NavBar = () => {
         </>
     )
     return (
-        <Navbar className={styles.NavBar} expand="md" fixed="top">
+        <Navbar className={styles.NavBar} expand="lg" fixed="top">
             <Container>
                 <NavLink to="/">
                     <Navbar.Brand>
                         <img src={logo} alt="logo" height="45" />
                     </Navbar.Brand>
                 </NavLink>
+                {currentUser && addAdvertIcon}
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="ml-auto text-left">
